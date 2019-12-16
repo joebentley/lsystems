d03684cd11639e651c9a2c1fb4df7f9666f4f668
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2019-12-16 18:28:54 +0000

    Fixed broken MathJax

--- project.clj
+++ project.clj
@@ -15,4 +15,4 @@
           ;; use MathJax so we can do LaTeX
           :html {:transforms [[:head] [:append
                                        [:script { :src "https://polyfill.io/v3/polyfill.min.js?features=es6"}]
-                                       [:script :async { :id "MathJax-script" :async true :src "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"}]]]}})
+                                       [:script { :id "MathJax-script" :async true :src "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"}]]]}})
