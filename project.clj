528d45748bff287dd132469d083106633b49bccf
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2019-12-18 14:17:07 +0000

    Started v0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lsystems "0.1.0"
+(defproject lsystems "0.2.0-SNAPSHOT"
   :description "Small and simple L-systems implementation in Clojure"
   :url "https://github.com/joebentley/lsystems"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
